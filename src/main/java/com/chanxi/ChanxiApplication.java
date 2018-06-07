b508f256aa2668a2bd4ae88345639e8176521fee
tanhao <tanhao@rongyiju.com>
2018-06-07 15:40:43 +0800

    第一次提交

---
+++ src/main/java/com/chanxi/ChanxiApplication.java
@@ -0,0 +1,12 @@
+package com.chanxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanxiApplication.class, args);
+	}
+}
